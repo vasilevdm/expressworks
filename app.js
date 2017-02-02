d8b86ed66d611340e2d079a03f31112c3dbc6c0f
Dmitry Vasilev <vasilev.dm@gmail.com>
2017-02-02 07:24:11 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var app = express()
+// app.get('/home', function(req,res){
+// 	res.end('Hello World!');
+// });
+console.log(process.argv)
+app.use(express.static(process.argv[3] || path.join(__dirname,'public')))
+app.listen(process.argv[2])
